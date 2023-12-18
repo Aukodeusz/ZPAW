5bc8e55ea6aef0a021b13ea6333fe3df47cd350c
Victor <twoj@email.com>
2023-12-18 19:39:35 +0100

    Dodano nowy plik

---
+++ App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import TaskList from './components/TaskList';
+
+function App() {
+  return (
+    <div>
+      <h1>Lista Zadań</h1>
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
